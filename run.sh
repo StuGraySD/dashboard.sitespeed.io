8228023a23b50bdca77fa40da75b9adcff117843
StuGraySD <53076896+StuGraySD@users.noreply.github.com>
2019-07-25 13:19:06 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -82,5 +82,5 @@ done
 # If you run a stable version (as YOU should), you don't need to remove the container
 docker system prune --all --volumes -f
 echo "Sleeping now"
-sleep 5m
+sleep 30m
 echo "Waking up"
